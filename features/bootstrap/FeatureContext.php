b5296882b75c16e82992412d3cedc7a06053e2e7
nirbhaypatil <nirbhaypatil@gmail.com>
2016-11-28 20:44:48 +0530

    behat v2.5  with mink example

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,58 @@
+<?php
+
+use Behat\Behat\Context\ClosuredContextInterface,
+    Behat\Behat\Context\TranslatedContextInterface,
+    Behat\Behat\Context\BehatContext,
+    Behat\Behat\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode,
+    Behat\Gherkin\Node\TableNode;
+	
+use Behat\Mink\Mink,
+    Behat\Mink\Session,
+    Behat\Mink\Driver\Selenium2Driver;
+
+require_once 'vendor/autoload.php';
+
+//
+// Require 3rd-party libraries here:
+//
+//   require_once 'PHPUnit/Autoload.php';
+//   require_once 'PHPUnit/Framework/Assert/Functions.php';
+//
+
+/**
+ * Features context.
+ */
+class FeatureContext extends BehatContext
+{
+    /**
+     * Initializes context.
+     * Every scenario gets its own context object.
+     *
+     * @param array $parameters context parameters (set them up through behat.yml)
+     */
+    public function __construct(array $parameters)
+    {
+        // Initialize your context here
+		
+		
+        $mink = new Mink(array(
+            'selenium2' => new Session(new Selenium2Driver($parameters['wd_capabilities']['browser'], $parameters['wd_capabilities'], $parameters['wd_host'])),
+        ));
+
+        $this->gui = $mink->getSession('selenium2');
+
+    }
+
+//
+// Place your definition and hook methods here:
+//
+//    /**
+//     * @Given /^I have done something with "([^"]*)"$/
+//     */
+//    public function iHaveDoneSomethingWith($argument)
+//    {
+//        doSomethingWith($argument);
+//    }
+//
+}
